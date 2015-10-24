f2e7d424ce8cbdac11846c81d588f55d4bcea0b5
Daniel Tsai <idaniel.twc@gmail.com>
2015-10-25 02:43:11 +0800

    add result, add form binding, add form js

--- src/js/form.js
+++ src/js/form.js
@@ -1,4 +1,7 @@
 $(document).ready( function(){
+  $('.ui.dropdown')
+      .dropdown()
+      ;
 	$(".start.button").click(function(){
 		$.fn.fullpage.moveSectionDown();
 	});
@@ -9,6 +12,22 @@ $(document).ready( function(){
 		$(".ui.form").submit();
 	});
 	$(".ui.radio.checkbox").checkbox();
+
+  $(".submit").click(function(){
+    var form={};
+    form.age=$("input[name=age]").val();
+    form.height=$("input[name=height]").val();
+    form.weight=$("input[name=weight]").val();
+    form.gender=$("input[name=gender]").val();
+    form.loc=$( "#loc" ).val();
+    form.smoke=$("input[name=smoke]").val();
+    form.wine=$("input[name=wine]").val();
+    form.sleep=$("input[name=sleep]").val();
+    form.vegetable=$("input[name=vegetable]").val();
+    form.exercise=$("input[name=exercise]").val();
+    form.exercise_hour=$("input[name=exercise_hour]").val();
+    form.Bvac=$("input[name=Bvac]").val();
+  });
 
 
 })
